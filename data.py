7a562de499c21c5378365e6e293f71177f774309
Vlad <vponojko@gmail.com>
2019-10-07 04:36:23 +0300

    more carefull code

---
+++ data.py
@@ -0,0 +1,87 @@
+from sqlalchemy import create_engine,Table, Column, Integer, String, ForeignKey, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.pool import QueuePool
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+import os
+
+engine = create_engine('mysql+mysqldb://root:123456@localhost:3306/cherry', echo = False, poolclass = QueuePool)
+Session = sessionmaker(bind = engine)
+Base = declarative_base()
+session = Session()
+class User(UserMixin,Base):
+	__tablename__ = 'user'
+
+	id = Column(Integer, primary_key=True, autoincrement = True)
+	nickname = Column(String(20), unique = True, nullable = False)
+	password = Column(String(100), nullable = False)
+	activity = Column(Integer)
+
+	post = relationship('Post', back_populates = 'user', cascade = "all, delete, delete-orphan")
+
+	def __init__(self, nickname, password, activity = 0):
+		self.nickname = nickname
+		self.password = generate_password_hash(password)
+		self.activity = activity
+	def __repr__(self):
+		return self.nickname  
+	def password_check(self, password):
+		return check_password_hash(self.password, password)
+
+class Post(Base):
+	__tablename__ = 'post'
+
+	post_id = Column(Integer, primary_key = True)
+	post_data = Column(String(255), nullable = False)
+	nickname = Column(String(20), ForeignKey('user.nickname'), nullable = False)
+
+
+	user = relationship("User", order_by = User.id, back_populates = 'post')
+
+	def __init__(self, post_data, User):
+		self.post_data = post_data
+		self.nickname = User.nickname
+		User.activity += 1
+
+	def __repr__(self):
+		return self.post_data + '@ by ' +self.nickname
+
+class Student(Base):
+	__tablename__ = 'student'
+
+	id = Column(Integer, primary_key=True, autoincrement = True)
+	name = Column(String(30), nullable = False)
+	surname = Column(String(30), nullable = False)
+	group = Column(String(30), nullable = False)
+	math = Column(Integer)
+	nature = Column(Integer)
+	programming = Column(Integer)
+
+	def __init__(self, name, surname, group, math = 0, nature = 0, programming = 0):
+		self.name = name
+		self.surname = surname
+		self.group = group
+		self.math = math
+		self.nature = nature
+		self.programming = programming
+ 
+	def __repr__(self):
+		return self.name + ' '  + self.surname
+
+
+class Disciplines(Base):
+	__tablename__ = 'disciplines'
+
+	id = Column(Integer, primary_key = True, autoincrement = True)
+	discipline = Column(String(30), nullable = True, unique = True)
+	name = Column(String(30), nullable = False, unique = True)
+	path = Column(String(70), nullable = False, unique = True )
+
+	def __init__(self, discipline, name, path = '/home/vlados/kurs/files'):
+		self.discipline = discipline
+		self.name = name
+		self.path = os.path.join(path + '/' + discipline, name)
+
+	def __repr__(self):
+		return self.discipline + ' : ' + self.name
---
+++ main.py
@@ -0,0 +1,96 @@
+from flask import Flask, render_template, Response, request, redirect, send_from_directory, url_for
+from data import User, Post, Student, Disciplines, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+from werkzeug.utils import secure_filename
+import os
+
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'gay'
+login_manager = LoginManager()
+login_manager.login_view ='login'
+login_manager.init_app(app)
+file_path = '/home/vlados/kurs/files'
+app.config['UPLOAD_FOLDER'] = file_path
+
+class LoginForm(FlaskForm):
+	username = StringField('username')
+	password = PasswordField('password')
+	submit = SubmitField('submit')
+
+
+
+@login_manager.unauthorized_handler
+@app.route('/login', methods = ['POST','GET'])
+def login():
+	form = LoginForm()
+	if request.method == 'POST':
+		x = session.query(User).filter(User.nickname == form.username.data).first()
+		if(x and x.password_check(form.password.data)):
+			login_user(x)
+			return redirect('title')
+		else:
+			return redirect('login')
+	return render_template('authorization.html')
+
+@app.route('/')
+@app.route('/title')
+def title():
+	return render_template('title.html',session = session, Post = Post, User = User, current_user = current_user)
+
+
+@app.route('/post', methods = ['POST','GET'])
+@login_required
+def to_post():
+	if request.method == 'POST':
+		session.add(Post(request.form['text'],current_user))
+		session.commit()
+		return redirect('title')
+	return render_template('post.html')
+
+
+@app.route('/materials')
+@login_required
+def materials():
+	return render_template('usefull_things.html', session = session, Disciplines = Disciplines)
+
+
+@app.route('/materials/upload', methods = ['POST','GET'])
+@login_required
+def materials_upload():
+	if request.method =='POST':
+		print(request.files)
+		f = request.files['filename']
+		f.save(os.path.join(app.config['UPLOAD_FOLDER'],f.filename))
+		return "file saved success"
+	return render_template('upload.html')
+
+
+@app.route('/download/<filename>')
+@login_required
+def download(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+
+@app.route('/group_list',methods = ['GET'])
+@login_required
+def group():
+	return render_template('group_list.html', session = session, Student = Student, current_user = current_user)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect('login')
+
+
+@login_manager.user_loader
+def load_user(id):
+	return session.query(User).filter(User.id == id).first()
+
+if (__name__='__main__'):
+	app.run()
